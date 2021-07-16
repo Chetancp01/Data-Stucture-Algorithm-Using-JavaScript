acc64e10b5fc7fac24f648ca5aae28f87b66e0dc
MehulPatel18 <patelmehul0018@gmail.com>
2021-07-16 17:49:44 +0530

    left-right pattern

---
+++ Pattern/RightTriangle.js
@@ -0,0 +1,21 @@
+/*
+Enter = 5; 
+*
+**
+***
+****
+*****
+
+*/
+let len = 5;
+for (let i = 0; i <= len; i++) {
+     let space = '';
+     let star = '';
+     for (let j = 0; j <= len; j++) {
+          if (j >= i)
+               space += ' ';
+          else
+               star += '*';
+     }
+     console.log(star + space);
+}
