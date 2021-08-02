7112c9fcf73d76d9c53aafeebbe9784d75d7dbc8
chetan <chetancp01@gmail.com>
2021-08-02 09:07:32 +0530

    Add New Arrays Sum

---
+++ ArraySum/primeNumber.js
@@ -0,0 +1,19 @@
+
+
+function primeNumber(num) {
+
+    for (var i = 2; i < num; i++) {
+
+        if (num%i === 0) {
+            console.log(num+" "+ "not prime number");
+            return false;
+        }
+
+        console.log(num+" "+ "prime number");
+        return true;
+
+    }
+
+}
+
+console.log(primeNumber(4));
