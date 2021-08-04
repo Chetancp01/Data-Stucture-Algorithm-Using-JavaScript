be3508ea541200080b7629171f974b43c9590150
MehulPatel18 <patelmehul0018@gmail.com>
2021-08-04 08:41:45 +0530

    Find minimum Distance between two numbers

--- ArraySum/Minimum_Distance.js
+++ ArraySum/Minimum_Distance.js
@@ -1,6 +1,6 @@
 // Find the minimum distance between two numbers
 
-
+// Method 1
 // time complexity o(n2)
 // function findMinimumDistancebtwTwoNum (arr, x, y,len) {
 //      let min_len = 0;
@@ -13,6 +13,34 @@
 //      }
 //      return min_len;
 // }
+
+
+// Method 2
+// time complexity o(n)
+function findMinimumDistancebtwTwoNum (arr, x, y, len) {
+     let i = 0;
+     let prev = undefined;
+     let min_len = len;
+     for ( ; i < len ; i++) {
+          if (arr[i] == x || arr[i] == y) {
+               prev = i;
+               break;
+          }
+     }
+     
+     for ( ; i < len; i++) {
+          if (arr[i] == x || arr[i] == y) {
+               if (arr[i] != arr[prev] && (i - prev) < min_len) {
+                    min_len = i - prev;
+                    prev = i;
+               } else {
+                    prev = i;
+               }
+          }
+     }
+     return min_len;
+}
+
 
 let arr = [3, 5, 4, 2, 6, 5, 6, 6, 5, 4, 8, 3];
 console.log(findMinimumDistancebtwTwoNum(arr, 3, 6, arr.length));
