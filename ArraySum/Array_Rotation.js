070c4e7a60118831eadde616e1f473f66af83f80
chetan <chetancp01@gmail.com>
2021-08-04 08:43:14 +0530

    pull by chetan

--- ArraySum/Array_Rotation.js
+++ ArraySum/Array_Rotation.js
@@ -1,5 +1,6 @@
 
-function gcd( a, b){
+function gcd(a, b){
+    console.log(a,b);
     if (b == 0)
         return a;
     else
@@ -9,7 +10,9 @@ function gcd( a, b){
 
 function leftRotate(arr, d, n){
     d = d % n;
+    console.log("D"+d);
     let g_c_d = gcd(d, n);
+    console.log("GCD"+g_c_d);
     for (let i = 0; i < g_c_d; i++) {
         let temp = arr[i];
         let j = i;
@@ -31,13 +34,15 @@ function leftRotate(arr, d, n){
  
 
 function printArray(arr, size){
-    for (let i = 0; i < size; i++)
-        console.log(arr[i] +" ");
+    for (let i = 0; i < size; i++){
+
+    }
+        // console.log(arr[i] +" ");
 }
  
 
 let arr = [ 1, 2, 3, 4, 5, 6, 7 ];
 let n = arr.length;
 
-leftRotate(arr, 2, n);
+leftRotate(arr, 3, n);
 printArray(arr, n);
